11fccdd76d7966a513383cb3b77037d228f48c43
Hao Nguyen <haonguyen17120612@gmail.com>
2025-09-07 12:22:19 +0000

    THUCHANH1

--- text2.cpp
+++ text2.cpp
@@ -1,40 +1,20 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
+// Hàm đếm số chữ số 0 tận cùng của N!
+int countTrailingZeros(long long n) {
+    long long count = 0;
+    for (long long i = 5; n / i >= 1; i *= 5) {
+        count += n / i;
+    }
+    return count;
+}
+
 int main() {
-    int N;
-    cin >> N; // Nhập số lượng phần tử cần có
-
-    vector<int> A; // Khai báo mảng chứa dãy số
-    int temp;
-    int count = 0;
-
-    // Đọc từng số và kiểm tra số lượng
-    while (cin >> temp) {
-        A.push_back(temp);
-        count++;
-        if (count > N) {
-            cout << "NO - số lượng phần tử vượt mức cho phép" << endl;
-            return 0;
-        }
-        else if (count < N) {
-        cout << "NO - số lượng phần tử bị thiếu" << endl;
-        return 0;
-        }
+    long long n;
+    // Nhập nhiều dòng n 
+    while (cin >> n) {
+        cout << countTrailingZeros(n) << endl;
     }
-
-
-    vector<bool> appeared(N + 1, false); // Mảng đánh dấu từ 1 đến N
-
-    for (int i = 0; i < N; ++i) {
-        if (A[i] < 1 || A[i] > N || appeared[A[i]]) {
-            cout << "NO" << endl;
-            return 0;
-        }
-        appeared[A[i]] = true;
-    }
-
-    cout << "YES" << endl;
     return 0;
 }
