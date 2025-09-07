e6575cebfdf87fc907f87c5e93e0ec5e24ff67d7
Hao Nguyen <haonguyen17120612@gmail.com>
2025-09-07 12:31:44 +0000

    THUCHANH1

--- text.cpp
+++ text.cpp
@@ -1,56 +1,46 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
+#include <unordered_set>
+#include <algorithm>
 using namespace std;
 
-// Hàm tạo danh sách các số nguyên tố ≤ 1000 
-vector<int> generatePrimes(int maxN) {
-    vector<bool> isPrime(maxN + 1, true); // Mảng động lưu số nguyên tố 
-    isPrime[0] = isPrime[1] = false;
-    for (int i = 2; i * i <= maxN; ++i) // Hàm loại bỏ bội số số nguyên tố 
-    {
-        if (isPrime[i]) {
-            for (int j = i * i; j <= maxN; j += i)
-                isPrime[j] = false;
-        }
-    }
-    vector<int> primes; // Lưu các 
-    for (int i = 2; i <= maxN; ++i) {
-        if (isPrime[i])
-            primes.push_back(i);
-    }
-    return primes;
-}
-
-// Hàm tính số lần xuất hiện của mỗi số nguyên tố trong N!
-vector<int> factorialPrimeExponents(int N, const vector<int>& primes) {
-    vector<int> exponents;
-    for (int p : primes) {
-        int count = 0;
-        int power = p;
-        while (power <= N) {
-            count += N / power;
-            power *= p;
-        }
-        exponents.push_back(count);
-    }
-    // Loại bỏ các số 0 ở cuối dãy
-    while (!exponents.empty() && exponents.back() == 0)
-        exponents.pop_back();
-    return exponents;
-}
-
 int main() {
-    vector<int> primes = generatePrimes(1000);
-    int N;
-    while (cin >> N) {
-        vector<int> result = factorialPrimeExponents(N, primes);
-        for (int i = 0; i < result.size(); ++i) {
-            cout << result[i];
-            if (i < result.size() - 1)
-                cout << " ";
+    int nx, ny, nz;
+    cin >> nx;
+    vector<int> x(nx);
+    for (int i = 0; i < nx; ++i) cin >> x[i];
+
+    cin >> ny;
+    vector<int> y(ny);
+    for (int i = 0; i < ny; ++i) cin >> y[i];
+
+    cin >> nz;
+    vector<int> z(nz);
+    for (int i = 0; i < nz; ++i) cin >> z[i];
+
+    // Dùng unordered_set để lưu phần tử của từng dãy
+    unordered_set<int> set_x(x.begin(), x.end());
+    unordered_set<int> set_y(y.begin(), y.end());
+    unordered_set<int> set_z(z.begin(), z.end());
+
+    vector<int> common;
+
+    // Duyệt qua set_x, nếu phần tử tồn tại trong cả set_y và set_z thì thêm vào kết quả
+    for (int num : set_x) {
+        if (set_y.count(num) && set_z.count(num)) {
+            common.push_back(num);
         }
-        cout << endl;
     }
+
+    // Sắp xếp kết quả tăng dần
+    sort(common.begin(), common.end());
+
+    // In kết quả
+    cout << common.size() << endl;
+    for (int num : common) {
+        cout << num << " ";
+    }
+    cout << endl;
+
     return 0;
 }
